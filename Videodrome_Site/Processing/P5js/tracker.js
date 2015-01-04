419584da2e320c1bc628de0016810f4e89fb51ce
Dylan Winter <dylanwinter@gmail.com>
2015-01-03 23:26:29 -0500

    Integrates videoframe-- still working on  getting callback to work

--- Videodrome_Site/Processing/P5js/tracker.js
+++ Videodrome_Site/Processing/P5js/tracker.js
@@ -1,23 +1,32 @@
 var rawData;
 var curFrame;
+var lastFrame;
+
 function preload(){
     rawData = loadJSON("parsedAEData.json");
-}
-
-function setup() {
-    createCanvas(1280, 720);
-    frameRate(24);
-    background(0, 0);
     curFrame = 0;
 }
 
+function setup() {
+    canvas = createCanvas(100, 100);
+    frameRate(24);
+    background(0, 0);
+    lastFrame = 0;
+}
+
 function draw() {
-    clear();
-    if(rawData[curFrame][0] != null && rawData[curFrame][1] != 0) {
+    clear(); // Clear to alpha
+    if(curFrame) lastFrame = curFrame; // if curframe exists, it is now the previous frame
+    curFrame = parseInt($("#time").text()); // get the new frame value
+    if (curFrame == lastFrame) curFrame++; // simulate movement if there's no new frame
+    //console.log("Target position: " + rawData[curFrame][0] + ", " + rawData[curFrame][1]);
+    if(rawData[curFrame][0] != null && rawData[curFrame][1] != 0) { // Draw
         noStroke();
         fill(255,0,0);
         ellipseMode(CENTER);
-        ellipse(rawData[curFrame][0], rawData[curFrame][1], 50, 50);
+        ellipse(width/2, height/2, 10, 10);
+        canvas.position(rawData[curFrame][0], rawData[curFrame][1]);
     }
-    curFrame++;
+
 }
+
